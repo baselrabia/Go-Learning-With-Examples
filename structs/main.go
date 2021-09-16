830c0c69126fb60830f9407ece9df4e96d2dc071
basel <basel.rabia@safavisa.com>
2021-09-16 16:44:38 +0200

    pointer with struct

---
+++ structs/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+
+
+type contactInfo struct {
+	email string
+	zip int
+}
+
+type person struct {
+	firstName string
+	lastName string
+	contact contactInfo
+}
+
+
+func main() {
+
+
+	alex := person{
+		firstName: "alex",
+		lastName: "anderson",
+		contact: contactInfo{
+			email: "alex@gmail.com",
+			zip: 94000,
+		},
+	}
+
+
+	alex.print()
+	alexPointer := &alex //define pointer 
+
+	alexPointer.updateName("jim")
+	alex.print()
+
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+	fmt.Println("")
+}
